2e1ade1f493e28cff7bb9c4a17c17f2b2801c785
perds <aperdec@gmail.com>
2016-11-08 14:59:28 -0500

    Initial commit. Created the application, added a Note pojo and added an activity to handle note editing.

---
+++ app/src/main/java/Note.java
@@ -0,0 +1,73 @@
+import java.sql.Blob;
+import java.util.Date;
+
+/**
+ * Created by Perds on 11/8/2016.
+ */
+
+public class Note {
+
+    private int id;
+    private String category;
+    private String text;
+    private String location;
+    private Date dateCreated;
+    private Blob picture;
+    private Blob audio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Blob getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Blob picture) {
+        this.picture = picture;
+    }
+
+    public Blob getAudio() {
+        return audio;
+    }
+
+    public void setAudio(Blob audio) {
+        this.audio = audio;
+    }
+}
---
+++ app/src/main/java/com/example/perds/morenotes/NoteEditing.java
@@ -0,0 +1,13 @@
+package com.example.perds.morenotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteEditing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editing);
+    }
+}
