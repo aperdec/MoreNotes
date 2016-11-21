2438d902d6d76e85bff310f3f26be27ff86ab204
perds <aperdec@gmail.com>
2016-11-21 16:27:37 -0500

    merged and formatted main menu

--- app/src/main/java/com/example/perds/morenotes/MainMenu.java
+++ app/src/main/java/com/example/perds/morenotes/MainMenu.java
@@ -1,20 +1,16 @@
 package com.example.perds.morenotes;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -102,9 +98,9 @@ public class MainMenu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void testing(){
-        Intent intent = new Intent(MainMenu.this,NoteEditing.class );
-       // MainMenu.this.startActivity(intent);
+    public void testing() {
+        Intent intent = new Intent(MainMenu.this, NoteEditing.class);
+        // MainMenu.this.startActivity(intent);
         startActivity(intent);
     }
 }
