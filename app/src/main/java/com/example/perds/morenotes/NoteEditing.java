d2773e6621ca18df0cfa78c367d0a5806d87b65e
Elviis <jonathansakowski@Gmail.com>
2016-11-21 15:29:14 -0500

    Edit Note page, Map Activity

--- app/src/main/java/com/example/perds/morenotes/NoteEditing.java
+++ app/src/main/java/com/example/perds/morenotes/NoteEditing.java
@@ -5,9 +5,32 @@ import android.os.Bundle;
 
 public class NoteEditing extends AppCompatActivity {
 
+    private final static int CONNECTION_FAILURE_REQUEST = 9000;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editing);
+
+        // text box
+        // location
+        // date
+        // picture
+        // audio
+
     }
+
+    public void getLocation(){
+        //get location
+
+    }
+
+    public void getDate(){
+        // get date
+
+    }
+
+
+
 }
