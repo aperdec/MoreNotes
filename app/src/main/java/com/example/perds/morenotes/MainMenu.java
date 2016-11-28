8feba0563369650321b68c8fa903dc63a44d8023
Elviis <jonathansakowski@Gmail.com>
2016-11-28 14:02:37 -0500

    Edit Note page, Map Activity

--- app/src/main/java/com/example/perds/morenotes/MainMenu.java
+++ app/src/main/java/com/example/perds/morenotes/MainMenu.java
@@ -2,30 +2,51 @@ package com.example.perds.morenotes;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.perds.morenotes.beans.Note;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.vision.text.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainMenu extends AppCompatActivity {
+import static android.R.attr.duration;
+import static com.example.perds.morenotes.R.id.text;
+
+public class MainMenu extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String NOTE_PREFS = "NotePrefs";
     private static final String SETTINGS_PREFS_NOTES = "SettingsPrefsNotes";
 
+
+    private GoogleApiClient googleApiClient;
+
+
     private ListView lstNotes;
 
     private List<Note> notes;
+
+    double lat, lng = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +56,7 @@ public class MainMenu extends AppCompatActivity {
         lstNotes = (ListView) findViewById(R.id.lstNotes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Intent intent = new Intent(MainMenu.this, NoteEditing.class);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +64,8 @@ public class MainMenu extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                onStart();
 
             }
         });
@@ -56,6 +80,13 @@ public class MainMenu extends AppCompatActivity {
         } else {
 
         }
+        //get address stuff
+       // addressTextView = (TextView) findViewById(R.id.txtLocation);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
     @Override
@@ -80,9 +111,68 @@ public class MainMenu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void testing(){
-        Intent intent = new Intent(MainMenu.this,NoteEditing.class );
-       // MainMenu.this.startActivity(intent);
-        startActivity(intent);
+    public void testing() {
+        //get address
+
+        //pass with intent
+
+        // MainMenu.this.startActivity(intent);
+
+        onStart();
+
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+
+     //  Toast toast = Toast.makeText(getApplicationContext(),"test", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Intent intent = new Intent(MainMenu.this, NoteEditing.class);
+        Location location = LocationServices.FusedLocationApi
+                .getLastLocation(googleApiClient);
+
+        if (location != null){
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            intent.putExtra("latitude", lat);
+            intent.putExtra("longitude", lng);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+
 }
